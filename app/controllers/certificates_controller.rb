c7993b5b49bbcb7ec113d3c2ffb8bc05a1bc677d
Geoffrey Wilson <g@wlsn.us>
2024-08-16 11:16:16 -0400

    abstract the certificate creation service

--- app/controllers/certificates_controller.rb
+++ app/controllers/certificates_controller.rb
@@ -2,7 +2,7 @@ class CertificatesController < ApplicationController
   def create
     name = params[:common_name] || "host.example.com"
     ttl = params[:ttl] || "24h"
-    cert = Services::VaultService.new.new_cert(name, ttl)
+    cert = Services::CertificateService.new.new_cert(name, ttl)
     render json: cert
   end
 end
