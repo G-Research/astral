82b46ce212675461fdf8e25a704c88c3367b597c
Geoff Wilson <geoff@gr-oss.io>
2024-08-20 10:45:04 -0400

    clarify test

--- test/integration/certificates_controller_test.rb
+++ test/integration/certificates_controller_test.rb
@@ -6,8 +6,8 @@ class CertificatesControllerTest < ActionDispatch::IntegrationTest
     assert_response :unauthorized
   end
 
-  test "create with faulty token" do
-    jwt = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJhcHBsaWNhdGlvbl9uYW1lIiwiY29tbW9uX25hbWUiOiJleGFtcGxlLmNvbSIsImlwX3NhbnMiOiIxMC4wLjEuMTAwIn1.61e0oQIj7vwGtOpFuPJDCI_Bqf8ZTpJxe_2kUwcbN7Y"
+  test "create with faulty token (encoded with different signing key)" do
+    jwt = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJhcHBsaWNhdGlvbl9uYW1lIiwiY29tbW9uX25hbWUiOiJleGFtcGxlLmNvbSIsImlwX3NhbnMiOiIxMC4wLjEuMTAwIn0.gEUyaZcARiBQNq2RUwZU0MdFXqthyo_oSQ8DAgKvxCs"
     post certificates_path, headers: { "Authorization" => "Bearer #{jwt}" }
     assert_response :unauthorized
   end
