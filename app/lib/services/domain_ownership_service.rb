49c2eda6106142c46ec68c444ee4414498de0243
Geoff Wilson <geoff@gr-oss.io>
2024-09-06 11:46:11 -0400

    remove extra private method

--- app/lib/services/domain_ownership_service.rb
+++ app/lib/services/domain_ownership_service.rb
@@ -2,19 +2,13 @@ module Services
   class DomainOwnershipService
     def authorize!(identity, cert_req)
       cert_req.fqdns.each do |fqdn|
-        domain = get_domain_name(fqdn)
+        domain = Domain.where(fqdn: fqdn).first
         raise AuthError unless domain.present? &&
                                (domain.owner == identity.subject ||
                                (domain.group_delegation &&
                                (domain.groups & identity.groups).any?))
       end
       nil
-    end
-
-    private
-
-    def get_domain_name(fqdn)
-      Domain.where(fqdn: fqdn).first
     end
   end
 end
