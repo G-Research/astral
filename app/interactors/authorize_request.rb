4397c5e9f58466d1959df1965ca5e9278ffde55a
Geoff Wilson <geoff@gr-oss.io>
2024-09-10 10:34:22 -0400

    Simplify domain ownership verificaiton

--- app/interactors/authorize_request.rb
+++ app/interactors/authorize_request.rb
@@ -4,10 +4,9 @@ class AuthorizeRequest
 
   def call
     context.request.fqdns.each do |fqdn|
-      domain = Domain.where(fqdn: fqdn).first
-      raise AuthError unless domain.present?
-      raise AuthError unless (domain.users_array & [ context.identity.subject ]).any? ||
-        (domain.group_delegation && (domain.groups_array & context.identity.groups).any?)
+      domain = Domain.where(fqdn: fqdn).first!
+      raise AuthError unless domain.users_array.include?(context.identity.subject) ||
+        (domain.group_delegation? && (domain.groups_array & context.identity.groups).any?)
     end
     nil
   end
