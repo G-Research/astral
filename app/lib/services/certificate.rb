41b8ce17b5735ea26b05a56862d6a968925dcaaf
Geoff Wilson <geoff@gr-oss.io>
2024-09-20 13:36:57 -0400

    Fix merge error

--- app/lib/services/certificate.rb
+++ app/lib/services/certificate.rb
@@ -1,24 +1,8 @@
 module Services
-<<<<<<<< HEAD:app/lib/services/secrets_service.rb
-  class SecretsService
-========
   class Certificate
->>>>>>>> main:app/lib/services/certificate.rb
     class << self
       def issue_cert(cert_issue_request)
         impl.issue_cert(cert_issue_request)
-      end
-
-      def kv_read(path)
-        impl.kv_read(path)
-      end
-
-      def kv_write(path, data)
-        impl.kv_write(path, data)
-      end
-
-      def kv_delete(path)
-        impl.kv_delete(path)
       end
 
       private
