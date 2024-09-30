9d656409954cede36e8b1f90602279dab32aa83c
Geoff Wilson <geoff@gr-oss.io>
2024-09-30 13:46:32 -0400

    Alter test to check root and and intermediate cert creation

--- test/lib/clients/vault_test.rb
+++ test/lib/clients/vault_test.rb
@@ -1,43 +1,51 @@
 require "test_helper"
 
 class VaultTest < ActiveSupport::TestCase
-  attr_reader :random_mount
+  attr_reader :intermedia_ca_mount
+  attr_reader :root_ca_mount
 
   setup do
     @client = Clients::Vault
-    @random_mount = SecureRandom.hex(4)
+    @root_ca_mount = SecureRandom.hex(4)
+    @intermedia_ca_mount = SecureRandom.hex(4)
   end
 
   teardown do
-    vault_client.sys.unmount(random_mount)
+    vault_client.sys.unmount(root_ca_mount)
+    vault_client.sys.unmount(intermedia_ca_mount)
   end
 
   test "#configure_kv" do
-    @client.stub :kv_mount, random_mount do
+    @client.stub :kv_mount, intermedia_ca_mount do
       assert @client.configure_kv
       engines = vault_client.sys.mounts
-      assert_equal "kv", engines[random_mount.to_sym].type
+      assert_equal "kv", engines[intermedia_ca_mount.to_sym].type
     end
   end
 
   test "#configure_pki" do
-    @client.stub :intermediate_ca_mount, random_mount do
-      assert @client.configure_pki
-      engines = vault_client.sys.mounts
-      assert_equal "pki", engines[random_mount.to_sym].type
+    @client.stub :root_ca_mount, root_ca_mount do
+      @client.stub :intermediate_ca_mount, intermedia_ca_mount do
+        assert @client.configure_pki
 
-      read_cert = vault_client.logical.read("#{random_mount}/cert/ca").data[:certificate]
-      assert_match "BEGIN CERTIFICATE", read_cert
+        [root_ca_mount, intermedia_ca_mount].each do |mount|
+          engines = vault_client.sys.mounts
+          assert_equal "pki", engines[mount.to_sym].type
 
-      cluster_config = vault_client.logical.read("#{random_mount}/config/cluster").data
-      assert_equal "#{vault_addr}/v1/#{random_mount}", cluster_config[:path]
-      assert_equal "#{vault_addr}/v1/#{random_mount}", cluster_config[:aia_path]
+          read_cert = vault_client.logical.read("#{mount}/cert/ca").data[:certificate]
+          assert_match "BEGIN CERTIFICATE", read_cert
 
-      role_config = vault_client.logical.read("#{random_mount}/roles/astral").data
-      assert_not_nil role_config[:issuer_ref]
-      assert_equal 720.hours, role_config[:max_ttl]
-      assert_equal true, role_config[:allow_any_name]
-     end
+          cluster_config = vault_client.logical.read("#{mount}/config/cluster").data
+          assert_equal "#{vault_addr}/v1/#{mount}", cluster_config[:path]
+          assert_equal "#{vault_addr}/v1/#{mount}", cluster_config[:aia_path]
+        end
+
+        role_config = vault_client.logical.read("#{intermedia_ca_mount}/roles/astral").data
+        assert_not_nil role_config[:issuer_ref]
+        assert_equal 720.hours, role_config[:max_ttl]
+        assert_equal true, role_config[:allow_any_name]
+      end
+    end
   end
 
   private
