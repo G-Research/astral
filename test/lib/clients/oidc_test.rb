3e9da0e719bbda5b56c36787af96303bf0451414
George Jahad <george@georgejahad.com>
2024-10-07 19:50:46 -0700

    comments

--- test/lib/clients/oidc_test.rb
+++ test/lib/clients/oidc_test.rb
@@ -8,29 +8,14 @@ class OIDCTest < ActiveSupport::TestCase
   test "#configure_oidc_user" do
     policy = <<-EOH
            path "sys" {
-           policy = "deny"
+           policy = "read"
            }
            EOH
-    @client.configure_oidc_user(Config[:initial_user][:name], Config[:initial_user][:email], policy)
+    @client.configure_oidc_user(Config[:initial_user][:name],
+                                Config[:initial_user][:email], policy)
     entity = @client.read_entity(Config[:initial_user][:name])
     assert_equal Config[:initial_user][:email], entity.data[:policies][0]
     aliases = entity.data[:aliases]
     assert aliases.find { |a| a[:name] == Config[:initial_user][:email] }
-  end
-
-  private
-  def vault_client
-    ::Vault::Client.new(
-          address: vault_addr,
-          token: vault_token
-    )
-  end
-
-  def vault_addr
-    Config[:vault_addr]
-  end
-
-  def vault_token
-    Config[:vault_token]
   end
 end
