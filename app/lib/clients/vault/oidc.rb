9d55d269bcf9402fd2f76fc43ad536d28a79c267
George Jahad <george@georgejahad.com>
2024-10-09 09:03:10 -0700

    cleanup

--- app/lib/clients/vault/oidc.rb
+++ app/lib/clients/vault/oidc.rb
@@ -2,78 +2,17 @@ module Clients
   class Vault
     module Oidc
       def configure_oidc_provider
-        # create oidc provider app
-        oidc_provider.logical.write(WEBAPP_NAME,
-                                    redirect_uris: "http://localhost:8250/oidc/callback",
-                                    assignments: "allow_all")
-        app = oidc_provider.logical.read(WEBAPP_NAME)
-        @@client_id = app.data[:client_id]
-        @@client_secret = app.data[:client_secret]
-
-        # create provider with email scope
-        oidc_provider.logical.write("identity/oidc/scope/email",
-                                    template: '{"email": {{identity.entity.metadata.email}}}')
-        oidc_provider.logical.write(Config[:oidc_provider][:name],
-                                    issuer: Config[:oidc_provider][:host],
-                                    allowed_client_ids: @@client_id,
-                                    scopes_supported: "email")
-
-        # create an entity for an initial user
-        oidc_provider.logical.write("identity/entity",
-                                    policies: "default",
-                                    name: Config[:initial_user][:name],
-                                    metadata: "email=#{Config[:initial_user][:email]}",
-                                    disabled: false)
-        provider = oidc_provider.logical.read(Config[:oidc_provider][:name])
-
-        # create userpass for initial user
-        oidc_provider.logical.delete("/sys/auth/userpass")
-        oidc_provider.logical.write("/sys/auth/userpass", type: "userpass")
-        oidc_provider.logical.write("/auth/userpass/users/#{Config[:initial_user][:name]}",
-                                    password: Config[:initial_user][:password])
-
-        # create an alias that maps the userpass to the entity
-        op_entity = oidc_provider.logical.read(
-          "identity/entity/name/#{Config[:initial_user][:name]}")
-        op_entity_id = op_entity.data[:id]
-        op_auth_list = oidc_provider.logical.read("/sys/auth")
-        up_accessor = op_auth_list.data[:"userpass/"][:accessor]
-        oidc_provider.logical.write("identity/entity-alias",
-                                    name: Config[:initial_user][:name],
-                                    canonical_id: op_entity_id,
-                                    mount_accessor: up_accessor)
+        create_provider_app
+        create_provider_with_email_scope
+        create_entity_for_initial_user
+        create_userpass_for_initial_user
+        create_alias_mapping_userpass_to_entity
       end
 
       def configure_oidc_client
-        client.logical.delete("/sys/auth/oidc")
-        client.logical.write("/sys/auth/oidc", type: "oidc")
-        issuer = "#{Config[:oidc_provider][:host]}/v1/#{Config[:oidc_provider][:name]}"
-        client.logical.write("auth/oidc/config",
-                                   oidc_discovery_url: issuer,
-                                   oidc_client_id: @@client_id,
-                                   oidc_client_secret: @@client_secret,
-                                   default_role: "reader")
-
-        # create default role that all oidc users will receive
-        policy = <<-EOH
-              path "sys" {
-              policy = "read"
-              }
-              EOH
-        client.sys.put_policy("reader", policy)
-        uris = <<-EOH
-             http://localhost:8200/ui/vault/auth/oidc/oidc/callback,
-             http://127.0.0.1:8200/ui/vault/auth/oidc/oidc/callback,
-             http://localhost:8250/oidc/callback,
-             http://127.0.0.1:8250/oidc/callback
-             EOH
-        client.logical.write(
-          "auth/oidc/role/reader",
-          bound_audiences: @@client_id,
-          allowed_redirect_uris: uris,
-          user_claim: "email",
-          oidc_scopes: "email",
-          token_policies: "reader")
+        create_client_config
+        create_default_policy_for_role
+        create_default_role
       end
 
       def configure_oidc_user(name, email, policy)
@@ -92,6 +31,90 @@ module Clients
           address: Config[:oidc_provider][:host],
           token: token
         )
+      end
+
+      def create_provider_app
+        oidc_provider.logical.write(WEBAPP_NAME,
+                                    redirect_uris: "http://localhost:8250/oidc/callback",
+                                    assignments: "allow_all")
+        app = oidc_provider.logical.read(WEBAPP_NAME)
+        @@client_id = app.data[:client_id]
+        @@client_secret = app.data[:client_secret]
+      end
+
+      def create_provider_with_email_scope
+        oidc_provider.logical.write("identity/oidc/scope/email",
+                                    template: '{"email": {{identity.entity.metadata.email}}}')
+        oidc_provider.logical.write(Config[:oidc_provider][:name],
+                                    issuer: Config[:oidc_provider][:host],
+                                    allowed_client_ids: @@client_id,
+                                    scopes_supported: "email")
+      end
+
+      def create_entity_for_initial_user
+        oidc_provider.logical.write("identity/entity",
+                                    policies: "default",
+                                    name: Config[:initial_user][:name],
+                                    metadata: "email=#{Config[:initial_user][:email]}",
+                                    disabled: false)
+      end
+
+      def create_userpass_for_initial_user
+        oidc_provider.logical.delete("/sys/auth/userpass")
+        oidc_provider.logical.write("/sys/auth/userpass", type: "userpass")
+        oidc_provider.logical.write("/auth/userpass/users/#{Config[:initial_user][:name]}",
+                                    password: Config[:initial_user][:password])
+      end
+
+      def create_alias_mapping_userpass_to_entity
+        op_entity = oidc_provider.logical.read(
+          "identity/entity/name/#{Config[:initial_user][:name]}")
+        op_entity_id = op_entity.data[:id]
+        op_auth_list = oidc_provider.logical.read("/sys/auth")
+        up_accessor = op_auth_list.data[:"userpass/"][:accessor]
+        oidc_provider.logical.write("identity/entity-alias",
+                                    name: Config[:initial_user][:name],
+                                    canonical_id: op_entity_id,
+                                    mount_accessor: up_accessor)
+      end
+
+      def create_client_config
+        client.logical.delete("/sys/auth/oidc")
+        client.logical.write("/sys/auth/oidc", type: "oidc")
+        issuer = "#{Config[:oidc_provider][:host]}/v1/#{Config[:oidc_provider][:name]}"
+        client.logical.write("auth/oidc/config",
+                                   oidc_discovery_url: issuer,
+                                   oidc_client_id: @@client_id,
+                                   oidc_client_secret: @@client_secret,
+                                   default_role: "reader")
+      end
+
+      def create_default_policy_for_role
+        policy = <<-EOH
+              path "sys" {
+              policy = "read"
+              }
+              EOH
+        client.sys.put_policy("reader", policy)
+      end
+
+      def get_redirect_uris
+        redirect_uris = <<-EOH
+             http://localhost:8200/ui/vault/auth/oidc/oidc/callback,
+             http://127.0.0.1:8200/ui/vault/auth/oidc/oidc/callback,
+             http://localhost:8250/oidc/callback,
+             http://127.0.0.1:8250/oidc/callback
+             EOH
+      end
+
+      def create_default_role
+        client.logical.write(
+          "auth/oidc/role/reader",
+          bound_audiences: @@client_id,
+          allowed_redirect_uris: get_redirect_uris,
+          user_claim: "email",
+          oidc_scopes: "email",
+          token_policies: "reader")
       end
     end
   end
