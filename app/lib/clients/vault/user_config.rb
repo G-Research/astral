abd9816ebc28e21a448c975518264c0733dd945b
George Jahad <george@georgejahad.com>
2024-10-24 15:49:25 -0700

    user_config

---
+++ app/lib/clients/vault/user_config.rb
@@ -0,0 +1,48 @@
+require "set"
+module Clients
+  class Vault
+    module UserConfig
+      def config_user(identity, cert)
+        sub = identity.sub
+        email = identity.email
+        entity = read_entity(sub)[:data]
+        policy = create_cert_policy(cert)
+        client.sys.put_policy(policy_name(sub), policy)
+        client.sys.put_policy(GENERIC_CERT_POLICY_NAME, generic_cert_policy)
+        policies = entity[:policies].append(policy_name(sub)).append(GENERIC_CERT_POLICY_NAME).to_set.to_a
+        put_entity(sub, policies, entity[:metadata])
+        put_entity_alias(sub, email , "oidc")
+      end
+
+      private
+
+      def create_cert_policy(cert)
+        "path \"#{intermediate_ca_mount}/cert/#{serial_number(cert)}\" {
+          capabilities = [\"read\"]
+        }"
+      end
+
+      def serial_number(cert)
+        cert[:serial_number]
+      end
+
+      def policy_name(sub)
+        "astral-#{sub}-cert-policy"
+      end
+
+      GENERIC_CERT_POLICY_NAME = "astral-generic-cert-policy"
+
+      def generic_cert_policy
+        policy = <<-EOH
+               path "#{cert_path}" {
+                 capabilities = ["create", "update"]
+               }
+
+               path "#{intermediate_ca_mount}/revoke" {
+                 capabilities = ["update"]
+               }
+        EOH
+      end
+    end
+  end
+end
