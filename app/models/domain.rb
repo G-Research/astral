1183b549509910c59e57de7db11ce461956eb16a
Geoff Wilson <geoff@gr-oss.io>
2024-10-25 10:50:56 -0400

    Add optional field encryption to Domain

--- app/models/domain.rb
+++ app/models/domain.rb
@@ -1,6 +1,11 @@
 class Domain < ApplicationRecord
   validates :fqdn, presence: true
 
+  if Config[:db_encryption]
+    encrypts :fqdn, :users, :groups
+  end
+
+
   def groups_array
     (groups || "").split(",").sort.uniq
   end
