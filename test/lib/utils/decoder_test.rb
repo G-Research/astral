a2a32e3c5d38634f21077e66fba81e066f2e1c80
George Jahad <george@georgejahad.com>
2024-10-29 14:03:35 -0700

    tests

--- test/lib/utils/decoder_test.rb
+++ test/lib/utils/decoder_test.rb
@@ -1,12 +1,17 @@
 require "test_helper"
+require_relative '../../../app/lib/utils/decoder_factory'
+require_relative '../../../app/lib/utils/jwks_decoder'
 
 class DecoderTest < ActiveSupport::TestCase
-  setup do
+  test "JwksDecoder returns correct identity" do
+    token = File.read("test/fixtures/files/token.jwks")
+    identity = JwksDecoder.new.decode(token)
+    assert_equal "john.doe@example.com", identity.sub
+    assert_equal "astral", identity.aud
   end
 
-  test "xxxxxxxxxxxxx" do
-    token = File.read("test/fixtures/files/token.jwks")
-    identity = Services::Auth.authenticate!(token)
+  test "DefaultDecoder returns correct identity" do
+    identity = DefaultDecoder.new.decode(jwt_authorized)
     assert_equal "john.doe@example.com", identity.sub
     assert_equal "astral", identity.aud
   end
