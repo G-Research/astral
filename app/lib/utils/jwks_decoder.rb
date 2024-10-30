a21bc460198e1e4540b41840c777ccf9238905b9
George Jahad <george@georgejahad.com>
2024-10-30 14:54:29 -0700

    cleanup

--- app/lib/utils/jwks_decoder.rb
+++ app/lib/utils/jwks_decoder.rb
@@ -8,16 +8,32 @@ class JwksDecoder
     @url = url
   end
 
+  # Decode a JWT token signed with JWKS
   def decode(token)
-    # Decode a JWT access token using the configured base.
-    jwks_hash = URI.open(@url) { |f| f.read }
-    jwks = JWT::JWK::Set.new(JSON.parse(jwks_hash))
-    jwks.filter! { |key| key[:use] == "sig" }
-    algorithms = jwks.map { |key| key[:alg] }.compact.uniq
+    jwks = get_jwks_keyset_from_configured_url
+
+    jwks = filter_out_non_signing_keys(jwks)
+
+    algorithms = get_algorithms_from_keyset(jwks)
     body = JWT.decode(token, nil, true, algorithms: algorithms, jwks: jwks)[0]
     Identity.new(body)
   rescue => e
     Rails.logger.warn "Unable to decode token: #{e}"
     nil
+  end
+
+  private
+
+  def get_jwks_keyset_from_configured_url
+    jwks_json = URI.open(@url) { |f| f.read }
+    JWT::JWK::Set.new(JSON.parse(jwks_json))
+  end
+
+  def filter_out_non_signing_keys(jwks)
+    jwks.filter { |k| k[:use] == "sig" }
+  end
+
+  def get_algorithms_from_keyset(jwks)
+    jwks.map { |k| k[:alg] }.compact.uniq
   end
 end
