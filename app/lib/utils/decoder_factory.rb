e503c6310af860ee4aa8f3eb232ec290004b5495
George Jahad <george@georgejahad.com>
2024-10-30 08:50:59 -0700

    cleanup

--- app/lib/utils/decoder_factory.rb
+++ app/lib/utils/decoder_factory.rb
@@ -3,8 +3,10 @@ require_relative "./jwks_decoder"
 class DecoderFactory
   cattr_reader :decoders
   class << self
+
     # Any new decoders should be added here:
     @@decoders = [JwksDecoder.new(Config[:jwks_url])]
+
     # Make sure default decoder comes last
     @@decoders.append(DefaultDecoder.new)
 
