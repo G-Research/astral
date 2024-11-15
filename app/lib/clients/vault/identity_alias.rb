a0f91a1e74fd6f636764d7d7f352ce5c89a491ec
Geoff Wilson <geoff@gr-oss.io>
2024-11-15 18:30:28 -0500

    WIP to check identity_alias before creation

--- app/lib/clients/vault/identity_alias.rb
+++ app/lib/clients/vault/identity_alias.rb
@@ -9,59 +9,50 @@ module Clients
         write_identity_alias("group", group_name, group_name, auth_method)
       end
 
-      def read_group_alias(group_name, alias_name)
-        id = read_entity_alias_id(group_name, alias_name)
+
+      def read_entity_alias(entity_name, alias_name, auth_path)
+        id = find_identity_alias_id("entity", entity_name, alias_name, auth_path)
         client.logical.read("identity/entity-alias/id/#{id}")
       end
 
-      def read_entity_alias_id(entity_name, alias_name)
-        e = read_entity(entity_name)
-        if e.nil?
-          raise "no such entity #{entity_name}"
-        end
-        aliases = e.data[:aliases]
-        a = aliases.find { |a| a[:name] == alias_name }
-        if a.nil?
-          raise "no such alias #{alias_name}"
-        end
-        a[:id]
-      end
-
-      def read_entity_alias(entity_name, alias_name)
-        id = read_entity_alias_id(entity_name, alias_name)
-        client.logical.read("identity/entity-alias/id/#{id}")
-      end
-
-      def delete_entity_alias(entity_name, alias_name)
-        id = read_entity_alias_id(entity_name, alias_name)
+      def delete_entity_alias(entity_name, alias_name, auth_path)
+        id = find_identity_alias_id("entity", entity_name, alias_name, auth_path)
         client.logical.delete("identity/entity-alias/id/#{id}")
       end
 
       private
 
-      def write_identity_alias(type, identity_name, alias_name  auth_method)
+      def find_identity_alias_id(type, identity_name, alias_name, auth_path)
+        e = read_identity("identity/#{type}/name/#{identity_name}")
+        if e.nil?
+          raise "no such #{type} #{identity_name}"
+        end
+        aliases = e.data[:aliases]
+        a = find_alias(aliases, alias_name, auth_path)
+        a&.fetch(:id)
+      end
+
+      def find_alias(aliases, name, auth_path)
+        aliases&.find { |a| a[:name] == name && a[:mount_path] == "auth/#{auth_path}/" }
+      end
+
+      def write_identity_alias(type, identity_name, alias_name, auth_method)
         auth_sym = "#{auth_method}/".to_sym
         accessor = client.logical.read("/sys/auth")
         accessor = accessor.data[auth_sym][:accessor]
-        alias_path = "identity/#{type}-alias/name/#{alias_name}"
-        result = client.logical.read(alias_path)
 
-        unless result
+        id = find_identity_alias_id(type, identity_name, alias_name, "oidc")
+        # only create alias when not existant
+        unless id
           identity = read_identity("identity/#{type}/name/#{identity_name}")
           client.logical.write("identity/#{type}-alias",
                                {
-                                 name: name,
+                                 name: alias_name,
                                  mount_accessor: accessor,
                                  canonical_id: identity.data[:id]
                                }
                               )
         end
-      rescue => e
-        # if redundant just ignore
-        if /already in use/.match? e.to_s
-          return
-        end
-        raise e
       end
     end
   end
