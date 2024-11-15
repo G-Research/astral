c9d0664d1e474b0c912f5d79b34427424d3db8c9
Geoffrey Wilson <g@wlsn.us>
2024-11-15 16:42:25 -0500

    dry up entity_alias and group_alias to use shared write_identity_alias

--- app/lib/clients/vault/identity_alias.rb
+++ app/lib/clients/vault/identity_alias.rb
@@ -2,21 +2,11 @@ module Clients
   class Vault
     module IdentityAlias
       def put_entity_alias(entity_name, alias_name, auth_method)
-        e = read_entity(entity_name)
-        if e.nil?
-          raise "no such entity #{entity_name}"
-        end
-        canonical_id = e.data[:id]
-        auth_sym = "#{auth_method}/".to_sym
-        accessor = client.logical.read("/sys/auth").data[auth_sym][:accessor]
-        client.logical.write("identity/entity-alias",
-                             name: alias_name,
-                             canonical_id: canonical_id,
-                             mount_accessor: accessor)
+        write_identity_alias("entity", entity_name, alias_name, auth_method)
       end
 
       def put_group_alias(group_name, auth_method)
-        write_identity_alias("group", group_name, auth_method)
+        write_identity_alias("group", group_name, group_name, auth_method)
       end
 
       def read_group_alias(group_name, alias_name)
@@ -49,15 +39,15 @@ module Clients
 
       private
 
-      def write_identity_alias(type, name, auth_method)
+      def write_identity_alias(type, identity_name, alias_name  auth_method)
         auth_sym = "#{auth_method}/".to_sym
         accessor = client.logical.read("/sys/auth")
         accessor = accessor.data[auth_sym][:accessor]
-        alias_path = "identity/#{type}-alias/name/#{name}"
+        alias_path = "identity/#{type}-alias/name/#{alias_name}"
         result = client.logical.read(alias_path)
 
         unless result
-          identity = read_identity("identity/#{type}/name/#{name}")
+          identity = read_identity("identity/#{type}/name/#{identity_name}")
           client.logical.write("identity/#{type}-alias",
                                {
                                  name: name,
