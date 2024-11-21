aebd7de7d487d35a17761d3f05ffc2db66c1847f
George Jahad <george@georgejahad.com>
2024-11-21 11:04:55 -0800

    more tests

--- test/interactors/application_interactor_test.rb
+++ test/interactors/application_interactor_test.rb
@@ -5,37 +5,34 @@ class ApplicationInteractorTest < ActiveSupport::TestCase
     @domain = domains(:owner_match)
     @identity = Identity.new(subject: @domain.users.first)
     @cr = Requests::CertIssueRequest.new(common_name: @domain.fqdn)
-    @log = Tempfile.new("log-test")
-    Config[:audit_log_file] = @log.path
   end
 
-  def teardown
-    @log.close
-    @log.unlink
+  test ".call will be logged as success" do
+    Object.const_set("SuccessAction", Class.new(ApplicationInteractor) do
+                       def call
+                       ensure
+                         audit_log
+                       end
+                     end)
+    rslt = SuccessAction.call(identity: @identity, request: @cr)
+    assert rslt.success?
+    log = SqlAuditLog.last
+    expected = {"action"=>"SuccessAction","result"=>"success","subject"=>"john.doe@example.com","cert_common_name"=>"example.com"}
+    assert expected <= log.attributes
   end
 
-  # test ".call will be logged as success" do
-  #   Object.const_set("SuccessAction", Class.new(ApplicationInteractor) do
-  #                      def call
-  #                      ensure
-  #                        audit_log
-  #                      end
-  #                    end)
-  #   rslt = SuccessAction.call(identity: @identity, request: @cr)
-  #   assert rslt.success?
-  #   assert_match %Q("action":"SuccessAction","result":"success","subject":"john.doe@example.com","cert_common_name":"example.com"), @log.readlines.last
-  # end
-
-  # test ".call will be logged as failure" do
-  #   Object.const_set("FailAction", Class.new(ApplicationInteractor) do
-  #                      def call
-  #                        context.fail!
-  #                      ensure
-  #                        audit_log
-  #                      end
-  #                    end)
-  #   rslt = FailAction.call(identity: @identity, request: @cr)
-  #   assert_not rslt.success?
-  #   assert_match %Q("action":"FailAction","result":"failure","subject":"john.doe@example.com","cert_common_name":"example.com"), @log.readlines.last
-  # end
+  test ".call will be logged as failure" do
+    Object.const_set("FailAction", Class.new(ApplicationInteractor) do
+                       def call
+                         context.fail!
+                       ensure
+                         audit_log
+                       end
+                     end)
+    rslt = FailAction.call(identity: @identity, request: @cr)
+    assert_not rslt.success?
+    log = SqlAuditLog.last
+    expected = {"action"=>"FailAction","result"=>"failure","subject"=>"john.doe@example.com","cert_common_name"=>"example.com"}
+    assert expected <= log.attributes
+  end
 end
